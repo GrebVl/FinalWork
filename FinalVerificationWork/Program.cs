9026f6859942f38babfb31ed5abb5785e2177017
Vladimir <chertonovskiy@icloud.com>
2022-09-04 18:47:10 +0700

    first commit FinalWork

---
+++ FinalVerificationWork/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Программа из массива строк формирует массив из строк, 
+длина которых меньше либо равна 3 символа.
+*/ 
+string[] array={"hello", "2", "world", ":)"};
+
+void PrintArray (string[] arr)
+{
+    for (int i=0; i<arr.Length; i++)
+    {
+        if(arr[i].Length<=3)
+        {
+            Console.Write(arr[i].Length+", ");
+        }
+    }
+}
+
+PrintArray(array);
